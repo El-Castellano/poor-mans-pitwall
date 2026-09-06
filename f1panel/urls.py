2f499a37241e3fd127f02c1a5ff574d9cf132a70
Lucas Castellano <90137000+El-Castellano@users.noreply.github.com>
2026-09-07 00:54:42 +0330

    Delete f1panel directory

--- f1panel/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import path, include
-
-urlpatterns = [
-    path("", include("timing.urls")),
-]
-
-urlpatterns += staticfiles_urlpatterns()
